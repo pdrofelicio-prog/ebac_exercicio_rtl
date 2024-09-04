d4b09f9beb6a95acc479271d0b7e51b7866e17cb
pdrofelicio-prog <pedro.felicio.silva@gmail.com>
2024-09-03 21:27:08 -0300

    Add files via upload

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
+import App from '../../App';
+
+import Comment from '../../models/Comment';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    test('Deve adicionar dois comentários', () => {
+        render(<App />)
+        fireEvent.change(screen.getByTestId('area-comment'), {
+            target: {
+                value: 'incrivel a sua miniatura'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        fireEvent.change(screen.getByTestId('area-comment'), {
+            target: {
+                value: 'aonde você comprou?'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+
+        expect(screen.getByText('incrivel a sua miniatura')).toBeInTheDocument()
+        expect(screen.getByText('aonde você comprou?')).toBeInTheDocument()
+    })
 });
